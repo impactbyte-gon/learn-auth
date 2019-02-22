0195aaf5ff4bc4d1d6289b2151a01603f6a112c0
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-22 17:41:00 +0700

    Put more comments

--- middlewares/users/index.js
+++ middlewares/users/index.js
@@ -14,20 +14,22 @@ const User = mongoose.model('User', {
   password: String
 })
 
-module.exports = {
+const usersMiddleware = {
+  // ---------------------------------------------------------------------------
+  // REGISTER NEW USER
   register: async (req, res) => {
-    // using the slow process is a slow process, so we use await
-    // destructure salt & password from encryptPassword() function
+    // using encryptPassword is a slow process, so we use await
+    // then destructure salt & password from encryptPassword() returned value
     const { salt, hashedPassword } = await helpers.encryptPassword(
       req.body.password
     )
 
     // creating an object is a fast process
     const newUser = {
-      name: req.body.name,
-      email: req.body.email,
-      salt: salt,
-      password: hashedPassword
+      name: req.body.name, // from body
+      email: req.body.email, // from body
+      salt: salt, // NOT from body, from helpers
+      password: hashedPassword // NOT from body, from helpers
     }
     // creating a user in the database is a slow process
     const result = await User.create(newUser)
@@ -40,28 +42,58 @@ module.exports = {
     })
   },
 
+  // ---------------------------------------------------------------------------
+  // LOGIN WITH REGISTERED USER
   login: async (req, res) => {
     res.send({
       message: 'Login'
     })
   },
 
+  // ---------------------------------------------------------------------------
   getAllUsers: async (req, res) => {
     res.send({
       message: 'Get all users',
-      users: await User.find()
+      users: await User.find({}, { salt: 0, password: 0 })
     })
   },
 
+  // ---------------------------------------------------------------------------
   getProfile: async (req, res) => {
     res.send({
       message: 'Get my profile'
     })
   },
 
+  // ---------------------------------------------------------------------------
   seedUsers: async (req, res) => {
+    const dummyUsersData = [
+      {
+        name: 'Alpha',
+        email: 'alpha@gmail.com',
+        password: 'password_alpha'
+      },
+      {
+        name: 'Beta',
+        email: 'beta@gmail.com',
+        password: 'password_beta'
+      },
+      {
+        name: 'Gamma',
+        email: 'gamma@gmail.com',
+        password: 'password_gamma'
+      }
+    ]
+
+    // do not use User.insertMany(dummyUsersData)
+    await dummyUsersData.forEach(async userData => {
+      await usersMiddleware.register(userData)
+    })
+
     res.send({
-      message: 'Seed users'
+      message: 'Seed users completed'
     })
   }
 }
+
+module.exports = usersMiddleware
